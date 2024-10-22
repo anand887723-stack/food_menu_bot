442dad64598e9341eb9be0f51fbf2a462af2e8c2
anand-887723-stack <anand887723@gmail.com>
2024-10-22 19:30:42 +0530

    deploying to heroku

--- app.py
+++ app.py
@@ -67,5 +67,10 @@ def whatsapp_reply():
 
     return str(response)
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
